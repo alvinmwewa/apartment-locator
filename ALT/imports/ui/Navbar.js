51ca90c28095d973a690d19ec24b63129def27b6
Jacquiline Chapoloko <jacchapoloko@gmail.com>
2018-09-25 08:24:30 +0200

    add materialize

--- ALT/imports/ui/Navbar.js
+++ ALT/imports/ui/Navbar.js
@@ -11,18 +11,22 @@ class Navbar extends Component{
 
 	render(){
 		return(
-			<div>
-				<div>
-					<a href='/'>Home</a>
-					<a href='/about'>About</a>
-					<a href='/contact'>Contact</a>
-					<a href='/login'>Log In</a>
-					<a href='/signup'>Sign Up</a>
+			<nav>
+				<div class="nav-wrapper">
+					<a href="#" class="brand-logo">Logo</a>
+					<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href='/'>Home</a></li>
+					<li><a href='/about'>About</a></li>
+					<li><a href='/contact'>Contact</a></li>
+					<li><a href='/login'>Log In</a></li>
+					<li><a href='/signup'>Sign Up</a></li>
+					</ul>
 				</div>
-			</div>
+			</nav>
 
 		)
 	}
 }
 
 export default Navbar
+        
