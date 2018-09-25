184364e389e02e21cdbd308b3b266eab2e4cc225
alvinmwewa <mwewamwewa1699@gmail.com>
2018-09-25 11:45:06 +0200

    message

--- ALT/imports/ui/Navbar.js
+++ ALT/imports/ui/Navbar.js
@@ -13,6 +13,15 @@ class Navbar extends Component{
 		return(
 			<nav>
 				<div class="nav-wrapper">
+				<ul id="slide-out" class="sidenav">
+    			<li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+    			<li><a href="#!">Second Link</a></li>
+    			<li><div class="divider"></div></li>
+    			<li><a class="subheader">Subheader</a></li>
+    			<li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  			</ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
 					<a href="#" class="brand-logo">Logo</a>
 					<ul id="nav-mobile" class="right hide-on-med-and-down">
 					<li><a href='/'>Home</a></li>
@@ -29,4 +38,3 @@ class Navbar extends Component{
 }
 
 export default Navbar
-        
